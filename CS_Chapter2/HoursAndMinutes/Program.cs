83f298fe0359a92523666a5b49000354677a9630
djbadore12 <djbadore12@yahoo.com>
2017-02-07 14:05:01 -0500

    Finished Chapter Two

--- CS_Chapter2/HoursAndMinutes/Program.cs
+++ CS_Chapter2/HoursAndMinutes/Program.cs
@@ -9,6 +9,11 @@ namespace HoursAndMinutes
     {
         static void Main(string[] args)
         {
+            int minutes = 345;
+            int hours = minutes / 60;
+            int remainder = minutes % 60;
+
+            Console.WriteLine("You worked for a total of {0} minuntes, or {1} hours and {2} minutes.", minutes hours, remainder);
         }
     }
 }
--- CS_Chapter2/Yards/Program.cs
+++ CS_Chapter2/Yards/Program.cs
@@ -9,6 +9,16 @@ namespace Yards
     {
         static void Main(string[] args)
         {
+            int length = 25;
+            int width = 42;
+
+            int total = length + width;
+            int squareFeet = total / 9;
+        
+            int cost = 14 * squareFeet;
+
+            Console.WriteLine("It will cost you $" + cost + " to carpet the room");
+            
         }
     }
 }
